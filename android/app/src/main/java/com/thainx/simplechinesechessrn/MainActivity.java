00189e795d4996af243478b044da7e087000e5c7
thainx <nxthai2910@gmail.com>
2021-09-05 22:08:36 +0700

    rename project

--- android/app/src/main/java/com/thainx/simplechinesechessrn/MainActivity.java
+++ android/app/src/main/java/com/thainx/simplechinesechessrn/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnstarter;
+package com.thainx.simplechinesechessrn;
 
 import android.os.Bundle;
 
@@ -20,7 +20,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "RNStarter";
+        return "SimpleChineseChessRN";
     }
 
     @Override
