b95c6cbea7be16a5fe76be1ce374d6e4d10cb400
thainx <nxthai2910@gmail.com>
2021-09-12 20:53:29 +0700

    feat(chess-game): integrate game engine

--- src/app/screens/game-screen/ChessGame.js
+++ src/app/screens/game-screen/ChessGame.js
@@ -1,8 +1,22 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useRef } from 'react'
+import { GameEngine } from 'react-native-game-engine'
 
-const ChessGame = () => {
-  return <View />
+// width: wWidth - 2 * 10
+
+function ChessGame() {
+  const engine = useRef()
+
+  return (
+    <GameEngine
+      ref={engine}
+      style={{
+        backgroundColor: 'burlywood',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      {/* <ChessBoard /> */}
+    </GameEngine>
+  )
 }
 
 export default ChessGame
